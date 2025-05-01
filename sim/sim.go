d1fc44479d5567c0869ffec9ad998a213eb1613f
Alexey Nilov <alexeyn00000@gmail.com>
2025-05-01 10:34:17 +0200

    CreateParticles

--- sim/sim.go
+++ sim/sim.go
@@ -2,13 +2,25 @@ package sim
 
 import (
 	"log"
+	"math/rand/v2"
+	"time"
 
 	"github.com/AlexeyNilov/space_sim/space"
 )
 
 func Run(space *space.Space) {
 	log.Println("Start sim")
-	pointID := 0
-	(*space)[pointID].CreateParticle("P")
+	time.Sleep(5 * time.Second)
+	CreateParticles(space, 0.2)
+}
 
+func CreateParticles(space *space.Space, probability float64) {
+	// Iterate over each point in the space
+	for i := range *space {
+		// Generate a random number between 0 and 1
+		if rand.Float64() < probability {
+			// Create a particle at the point if the random number is less than the probability
+			(*space)[i].CreateParticle("P")
+		}
+	}
 }
--- sim/sim_test.go
+++ sim/sim_test.go
@@ -1,1 +1,41 @@
 package sim
+
+import (
+	"testing"
+
+	"github.com/AlexeyNilov/space_sim/space"
+	"github.com/stretchr/testify/assert"
+)
+
+const spaceSize = 10
+
+func TestCreateParticles(t *testing.T) {
+	// Helper function to count points with particles
+	countParticles := func(s *space.Space) int {
+		count := 0
+		for _, point := range *s {
+			if point.PointsTo != nil {
+				count++
+			}
+		}
+		return count
+	}
+
+	tests := []struct {
+		name       string
+		probability float64
+		expected    int
+	}{
+		{"AllParticles", 1, spaceSize}, // Probability = 1, expect all points to have particles
+		{"NoParticles", 0, 0},         // Probability = 0, expect no points to have particles
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := space.CreateSpace(spaceSize)
+			CreateParticles(s, tt.probability)
+			assert.Equal(t, tt.expected, countParticles(s))
+		})
+	}
+}
+
